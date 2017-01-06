04b7dc940480a6cfe77d28b67311b7bdcb94bec3
Malika Butler <malika@vimeo.com>
2017-01-06 17:28:27 -0500

    clean up spacing in config variables

--- config.js
+++ config.js
@@ -1,12 +1,7 @@
 const path = require('path');
 const LOCAL_PATH = path.resolve(__dirname)
-<<<<<<< HEAD:config.js
-const LOCAL_DATA = LOCAL_PATH+ '/data/';
-const COMPONENT_DATA = LOCAL_DATA +'componentAPI.json';
-=======
 const LOCAL_DATA = LOCAL_PATH + '/../data/';
 const COMPONENT_DATA = LOCAL_DATA + 'componentAPI.json';
->>>>>>> setup js file for json to sass token conversion:gulp-tasks/config.js
 
 
 module.exports = {
