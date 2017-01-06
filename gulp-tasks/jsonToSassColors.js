bf83b622944383e4c3ef35baaa3bd9ef0c813fd0
Malika Butler <malika@vimeo.com>
2017-01-06 17:23:11 -0500

    setup js file for json to sass token conversion

---
+++ gulp-tasks/jsonToSassColors.js
@@ -0,0 +1,7 @@
+const gulp = require('gulp');
+const config = require('./config');
+
+// Clear out build folders for fresh builds
+gulp.task('jsonToSassColors', function () {
+	console.log("convert json to sass color variables");
+});
