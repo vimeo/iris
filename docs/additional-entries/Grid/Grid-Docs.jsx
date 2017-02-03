d0d782f8437f3ce1fcedff08dff5dffe0581afa2
Malika Butler <malika@vimeo.com>
2017-02-02 19:08:10 -0500

    Set-up basic 24 column grid.

--- docs/additional-entries/Grid/Grid-Docs.jsx
+++ docs/additional-entries/Grid/Grid-Docs.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Grid from '../../../src/components/Grid/Grid.jsx';
 import Block from '../../../src/components/Block/Block.jsx';
+import Col from '../../../src/components/Col/Col.jsx';
 import ExampleSource from 'steadicam/components/styleListings/ExampleSource/ExampleSource';
 
 export default function GridDocs() {
@@ -12,12 +13,65 @@ export default function GridDocs() {
 			<div data-code>
 				<Grid>
 
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+						<Col span={1}></Col>
+
 				</Grid>
 			</div>
 			<ExampleSource>
 				{`
 					<Grid>
+						<Col span={8}></Col>
+						<Col span={16} nested>
 
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+								<Col span={1}></Col>
+							
+						</Col>
 					</Grid>
 				`}
 			</ExampleSource>
