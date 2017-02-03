7464e23c68b25c9c603a15f2eb19b0f63ac21f8d
Malika Butler <malika@vimeo.com>
2017-02-02 19:09:23 -0500

    Accept col keywords and trigger aside element for rail prop.

--- src/components/Col/Col.jsx
+++ src/components/Col/Col.jsx
@@ -4,14 +4,17 @@ import styles from './Col.css';
 
 const displayName = 'Col';
 
-
+const spanTypes = React.PropTypes.oneOfType([
+    React.PropTypes.number,
+    'half', 'quarter', 'third'
+]);
 
 const propTypes =  {
-    span: React.PropTypes.number,
-    lgSpan: React.PropTypes.number,
-    mdSpan: React.PropTypes.number,
-    smSpan: React.PropTypes.number,
-    order: React.PropTypes.number,
+    span: spanTypes,
+    lgSpan: spanTypes,
+    mdSpan: spanTypes,
+    smSpan: spanTypes,
+    nested: React.PropTypes.bool,
     rail: React.PropTypes.bool,
     className: React.PropTypes.string,
     offset: React.PropTypes.number
@@ -24,29 +27,57 @@ const defaultProps = {
 class Col extends React.Component {
 
   render () {
+
+    //set up offset class naming
+    let offsetType;
+
+    if(this.props.offset && this.props.offset >= 1) {
+        offsetType = 'plus-';
+    } else if(this.props.offset && this.props.offset <= 1) {
+        offsetType = 'negative-';
+    }
+
     // className builder
     const colClass = classNames(
-      styles.Col,
-      this.props.className
+        styles.Col,
+        styles['span-' + this.props.span],
+        (this.props.lgSpan ? styles['offset-' + this.props.lgSpan]: null),
+        (this.props.mdSpan ? styles['offset-' + this.props.mdSpan]: null),
+        (this.props.smSpan ? styles['offset-' + this.props.smSpan]: null),
+        (this.props.offset ? styles['offset-' + offsetType + this.props.offset]: null),
+        (this.props.nested ? styles['nested']: null),
+        styles[this.props.align],
+        this.props.className
     );
 
     // filter out presentational props from this.props and store the rest as "filteredProps" to be printed into the component as properties (e.g. HTML attribute pass-through, event handlers)
     const {
-        offset,
-        order,
-        className,
         children,
+        rail,
         filteredProps
     } = this.props;
 
-    return (
-      <div
-        {...filteredProps}
-        className={colClass}
-      >
-        {this.props.children}
-      </div>
-    );
+    let colContent = this.props.children;
+
+    if(!rail) {
+        return (
+          <div
+            {...filteredProps}
+            className={colClass}
+          >
+            {colContent}
+          </div>
+        );
+    } else {
+        return (
+          <aside
+            {...filteredProps}
+            className={colClass}
+          >
+            {colContent}
+        </aside>
+        );
+    }
   }
 
 };
