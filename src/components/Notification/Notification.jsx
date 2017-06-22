b804e0254eafe2d1372a842a2625fc07be434244
Jory Cunningham <jory@vimeo.com>
2017-06-22 17:58:20 -0400

    Launch Iris Modals (#77)
    * Iris Modals (#68)
    
    * bump version
    
    * add back the delete icon
    
    * repair prop listing on notifications
    
    * modals and ButtonDialogDismiss
    
    * fix modal demo toggle functions
    
    * modal PR feedback
    
    * more specific flowtypes for modal members
    
    * protect thisEL in modal
    
    * add non-dismissible mode
    
    * freeze scroll when modal is open, add scroll to modal window
    
    * add fullBleed to modal
    
    * unlock body based on isShowing in modal
    
    * add unmount listener to modal to clean up
    
    * make onDismiss the trigger for dismissibility
    
    * make onDismiss the trigger for dismissibility, fix flow error
    
    * bump version in package
    
    * Add componentDidMount method to Modal (#71)
    
    * bump version
    
    * add back the delete icon
    
    * repair prop listing on notifications
    
    * add componentDidUpdate method to open modal on mount
    
    * update lifecycle order
    
    * increase modal z-index
    
    * Fix modal-index and scrolling (#76)
    
    * bump version
    
    * add back the delete icon
    
    * repair prop listing on notifications
    
    * add componentDidUpdate method to open modal on mount
    
    * update lifecycle order
    
    * increase modal z-index
    
    * update z-index and scroll behavior

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import classNames from 'classnames';
 import styles from './Notification.scss';
-import DeleteIcon from '../../globals/svg/dismiss_iris.svg';
+import ButtonDialogClose from '../ButtonDialogClose/ButtonDialogClose';
+
 import animationStyles from '../../animations/dropInOut/dropInOut.scss';
 
 /* Note: This component is the base component for themed notifications, do not use it by itself */
@@ -44,10 +45,17 @@ const Notification = (props: Props): React$Element<*> => {
         </span>
     );
 
+    const dismissElementClass = classNames(
+        styles.dismissButton,
+        (dismissIconClassName ? dismissIconClassName : null)
+    );
+
     const dismissElement = (
-            <button className = {styles.dismissButton} onClick={onDismiss}>
-                <DeleteIcon title="Dismiss this notification" className={dismissIconClassName} />
-            </button>
+            <ButtonDialogClose
+                className={dismissElementClass}
+                onClick={onDismiss}
+                buttonTitle="Dismiss this notification"
+             />
     );
 
     return (
