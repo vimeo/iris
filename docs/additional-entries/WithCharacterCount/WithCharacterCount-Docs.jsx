3ca7d776799accd403a9a49f3c62d128f33b00c3
Jory Cunningham <jory@vimeo.com>
2017-11-14 11:38:42 -0500

    doc import typo

--- docs/additional-entries/WithCharacterCount/WithCharacterCount-Docs.jsx
+++ docs/additional-entries/WithCharacterCount/WithCharacterCount-Docs.jsx
@@ -4,7 +4,7 @@ import InputText from '../../../src/InputText';
 import TextArea from '../../../src/TextArea';
 import {Header4, ParagraphMd} from '../../../src/Type';
 import {List, ListItem} from '../../../src/List';
-import withCharacterCount from '../../../src/withCharacterCounter';
+import withCharacterCount from '../../../src/withCharacterCount';
 
 const InputTextWithCharacterCount = withCharacterCount(InputText);
 const TextAreaWithCharacterCount = withCharacterCount(TextArea);
