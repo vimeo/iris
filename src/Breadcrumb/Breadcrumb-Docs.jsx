bf7340fb5f7c3ee88d9508f1673acbdfbdc88511
Jory Cunningham <jory@vimeo.com>
2017-11-14 10:23:02 -0500

    add docs

--- src/Breadcrumb/Breadcrumb-Docs.jsx
+++ src/Breadcrumb/Breadcrumb-Docs.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import Breadcrumb from './Breadcrumb';
 import ExampleSource from 'steadicam/components/styleListings/ExampleSource/ExampleSource';
-import { ParagraphMd } from '../Type';
+import { ParagraphMd, ParagraphAltMd } from '../Type';
 
 class BreadcrumbDocs extends React.Component {
     render() {
         return (
             <div className="Pattern__docs">
                 <ParagraphMd>Breadcrumbs help orient the user within an page flow.</ParagraphMd>
+                <ParagraphMd>"Crumb" links are passed as an array of object to <code>crumbs</code> and the current page label should be passed to <code>currentPageLabel</code> as a string.</ParagraphMd>
+
                 <div data-code>
                     <Breadcrumb
                         crumbs = {[
                             {
                                 label: 'Breadcrumb Item 1 is Pretty Long So Cut it Off',
                                 to: '#',
+                                onClick: ()=>{
+                                    console.log('click');
+                                },
                             },
                             {
                                 label: 'Breadcrumb Item 2',
@@ -26,7 +31,20 @@ class BreadcrumbDocs extends React.Component {
 
                 <ExampleSource>
                     {`
-                        <Breadcrumb />
+<Breadcrumb
+    crumbs = {[
+        {
+            label: 'Breadcrumb Item 1 is Pretty Long So Cut it Off',
+            to: '#',
+            onClick: ()=>{console.log('click')}
+        },
+        {
+            label: 'Breadcrumb Item 2',
+            to: '#',
+        },
+    ]}
+    currentPageLabel="Current Page Label is Pretty Long So Cut it Off"
+/>
                         `}
                     </ExampleSource>
                 </div>
