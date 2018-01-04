a615f934a7079681015285681786be0ed23e9fd8
Jory Cunningham <jory@vimeo.com>
2018-01-04 12:03:18 -0500

    update loader docs

--- src/LoaderCircular/LoaderCircular-Docs.jsx
+++ src/LoaderCircular/LoaderCircular-Docs.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import LoaderCircular from './LoaderCircular';
 import ExampleSource from 'steadicam/components/styleListings/ExampleSource/ExampleSource';
-
+import { ParagraphMd, Header4 } from '../Type';
 const LoaderCircularDocs = (props) => {
     return (
         <div>
             <div data-code>
-                <LoaderCircular />
+                <Header4>Dark</Header4>
+                <LoaderCircular format="dark" />
+                <div className="Pattern-DarkBlock">
+                    <Header4 format="light">Light</Header4>
+                    <LoaderCircular format="light"/>
+                </div>
+                <div style={{ color: '#d93636' }}>
+                    <Header4>Adaptive</Header4>
+                    <ParagraphMd>This div has the style set to <code>color: '#d93636'</code></ParagraphMd>
+                    <LoaderCircular format="adaptive"/>
+                </div>
             </div>
 
             <ExampleSource>
                 {`
-                    <LoaderCircular />
+<LoaderCircular format="dark" />
+<LoaderCircular format="light"/>
+<div style={{ color: '#d93636' }}>
+    <LoaderCircular format="adaptive"/>
+</div>
                     `}
                 </ExampleSource>
             </div>
