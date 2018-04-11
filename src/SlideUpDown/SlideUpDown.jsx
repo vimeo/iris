7e0ce59fedb3293553418e28c5717dda01102014
Jory Cunningham <jory@vimeo.com>
2018-04-11 17:23:24 -0400

    Iris 3.3.7

--- src/SlideUpDown/SlideUpDown.jsx
+++ src/SlideUpDown/SlideUpDown.jsx
@@ -83,7 +83,6 @@ class SlideUpDown extends React.Component {
             },
             entered: {
                 maxHeight: rem(this.state.maxHeight),
-                overflowY: 'visible',
             },
             exiting: {
                 maxHeight: 0,
