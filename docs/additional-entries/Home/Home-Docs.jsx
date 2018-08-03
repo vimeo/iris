5d6d7de1adf326171632f60616b88a6dc99f1951
Jory Cunningham <jory@vimeo.com>
2018-08-03 16:28:43 -0400

    add more docs notes

--- docs/additional-entries/Home/Home-Docs.jsx
+++ docs/additional-entries/Home/Home-Docs.jsx
@@ -4,6 +4,7 @@ import {
     Header3,
     ParagraphMd,
     LinkText,
+    NotificationWarning,
 } from '../../../src';
 const HomeDocs = (props) => {
     return (
@@ -16,6 +17,12 @@ const HomeDocs = (props) => {
                     import {Button} from '@vimeo/iris';
                 `}
             </ExampleSource>
+            <NotificationWarning headerText="Don't Override Components">
+                <ParagraphMd>Iris components are developed in a live-styleguide environment. All variants of the component can be modelled in this environment and it is used to ensure that high-quality, accesibile, and performant components are delivered.</ParagraphMd>
+                <ParagraphMd>When there are overrides to the way a component looks or functions future enhancements, including high-priority bug fixes, can cause unforeseen issues as the changes are only tested against their intended use.</ParagraphMd>
+                <ParagraphMd>Required component enhancements can be submitted to Iris via PR (see below)</ParagraphMd>
+            </NotificationWarning>
+
             <Header3>Contributing to Iris</Header3>
             <ParagraphMd>To open a PR or report a bug, visit the <LinkText href="https://github.vimeows.com/Vimeo/iris" target="_blank">Iris GitHub Repo</LinkText>.</ParagraphMd>
             <Header3>Getting Help</Header3>
