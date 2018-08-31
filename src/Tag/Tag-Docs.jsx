87fcbd447e2466dc1b2d7d22a3db0116b30b7057
Sean McIntyre <sean.mcintyre@vimeo.com>
2018-08-31 12:32:50 -0400

    add more tags to docs (#424)

--- src/Tag/Tag-Docs.jsx
+++ src/Tag/Tag-Docs.jsx
@@ -16,12 +16,20 @@ class TagDocs extends React.Component {
             <div className="Pattern__docs">
                 <ParagraphMd>Tags are button-like elements that show taxonomical tags</ParagraphMd>
                 <div data-code>
+                    <Tag size="xs">Documentary</Tag>
                     <Tag size="sm">Animation</Tag>
                     <Tag size="md">Narrative</Tag>
                     <Tag size="lg">Comedy</Tag>
+                    <Tag
+                        onDismiss={this._dismissHandler}
+                        size="lg"
+                    >
+                        Comedy
+                    </Tag>
                 </div>
 
                 <div className="Pattern-DarkBlock">
+                    <Tag format="dark" size="xs">Documentary</Tag>
                     <Tag format="dark" size="sm">Animation</Tag>
                     <Tag format="dark" size="md">Narrative</Tag>
                     <Tag format="dark" size="lg">Comedy</Tag>
@@ -36,9 +44,11 @@ class TagDocs extends React.Component {
 
                 <ExampleSource>
                     {`
+<Tag size="xs">Documentary</Tag>
 <Tag size="sm">Animation</Tag>
 <Tag size="md">Narrative</Tag>
 <Tag size="lg">Comedy</Tag>
+<Tag format="dark" size="xs">Documentary</Tag>
 <Tag format="dark" size="sm">Animation</Tag>
 <Tag format="dark" size="md">Narrative</Tag>
 <Tag format="dark" size="lg">Comedy</Tag>
