36d815d1c291ff8253672c263bfdb03316716180
Sean McIntyre <sean.mcintyre@vimeo.com>
2018-10-01 10:52:49 -0400

    add empty cart icon (and fix svg page) (#550)
    * add empty cart icon (and fix svg page)
    
    * minor adjustments

--- docs/apps/svgPage/svgPage.jsx
+++ docs/apps/svgPage/svgPage.jsx
@@ -6,37 +6,32 @@ import * as Illustrations from './svgIllustrationExportList.js';
 const svgData = require('json-loader!../../../data/svgIconList.json');
 const illustrationData = require('json-loader!../../../data/svgIllustrationsList.json');
 
-const SvgList = () => {
-    return (
-        <div>
-            <h2 id="icons">Icons</h2>
-            {svgData.map(function (icon, i) {
+const SvgList = () => (
+    <div>
+        <h2 id="icons">Icons</h2>
+        {svgData.map((icon, i) => (
+            <SvgEntry iconName={icon.name} fileName={icon.filename} key={i} />
+        ))}
+        <h2 id="illustrations">Illustrations</h2>
+        {illustrationData.map((icon, i) => (
+            <IllustrationEntry
+                iconName={icon.name}
+                fileName={icon.filename}
+                key={i}
+            />
+        ))}
+    </div>
+);
 
-                return <SvgEntry
-                    iconName = {icon.name}
-                    fileName = {icon.filename}
-                    key = {i}
-                />
-            })}
-            <h2 id="illustrations">Illustrations</h2>
-            {illustrationData.map(function (icon, i) {
-
-                return <IllustrationEntry
-                    iconName = {icon.name}
-                    fileName = {icon.filename}
-                    key = {i}
-                />
-            })}
-        </div>
-    )
-};
-
-const SvgEntry = (props) => {
-    let IconTag = Icons[props.iconName];
+const SvgEntry = ({
+    iconName,
+    fileName
+}) => {
+    const IconTag = Icons[iconName];
     return (
         <div className="sg-svgEntry">
-                <h3>{props.iconName}</h3>
-                <div className="sg-svgIcon sg-svgIcon--xlarge">
+            <h3>{iconName}</h3>
+            <div className="sg-svgIcon sg-svgIcon--xlarge">
                 <IconTag />
             </div>
             <div className="sg-svgIcon sg-svgIcon--large">
@@ -52,42 +47,50 @@ const SvgEntry = (props) => {
                 <IconTag />
             </div>
             <div className="sg-svgIconInfo">
-                <p><strong>ComponentName:</strong> {props.iconName}</p>
-                <p><strong>File Name:</strong> {props.fileName}.svg</p>
+                <p>
+                    <strong>ComponentName:</strong> {iconName}
+                </p>
+                <p>
+                    <strong>File Name:</strong> {fileName}
+                    .svg
+                </p>
             </div>
         </div>
-    )
+    );
 };
 
-const IllustrationEntry = (props) => {
-    let IconTag = Illustrations[props.iconName];
+const IllustrationEntry = ({
+    iconName,
+    fileName
+}) => {
+    let IconTag = Illustrations[iconName];
     return (
         <div className="sg-svgEntry">
-            <h3>{props.iconName}</h3>
+            <h3>{iconName}</h3>
             <div className="sg-svgIllustration">
-            <IconTag />
+                <IconTag />
             </div>
             <div className="sg-svgIconInfo">
-                <p><strong>ComponentName:</strong> {props.iconName}</p>
-                <p><strong>File Name:</strong> {props.fileName}.svg</p>
+                <p>
+                    <strong>ComponentName:</strong> {iconName}
+                </p>
+                <p>
+                    <strong>File Name:</strong> {fileName}.svg
+                </p>
             </div>
         </div>
-    )
+    );
 };
 
-
-const App = () => {
-    return (
-        <div className="sg-svgIconListWrapper">
-            <h1>Svgs</h1>
-            <p>Jump to: <a href='#icons'>icons</a> | <a href='#illustrations'>illustrations</a></p>
-            <SvgList />
-        </div>
-    )
-};
-
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('app-root')
+const App = () => (
+    <div className="sg-svgIconListWrapper">
+        <h1>Svgs</h1>
+        <p>
+            Jump to: <a href="#icons">icons</a> |{' '}
+            <a href="#illustrations">illustrations</a>
+        </p>
+        <SvgList />
+    </div>
 );
+
+ReactDOM.render(<App />, document.getElementById('app-root'));
