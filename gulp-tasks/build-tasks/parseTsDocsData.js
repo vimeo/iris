023d8f8e9f212a60a80d818c7ef745bae8539d86
Sean McIntyre <sean.mcintyre@vimeo.com>
2018-11-13 18:38:26 -0500

    blacklist unnecessary exports (temporary solution)

--- gulp-tasks/build-tasks/parseTsDocsData.js
+++ gulp-tasks/build-tasks/parseTsDocsData.js
@@ -1,12 +1,13 @@
 const parseTsDocsData = () => {
     const patternData = require("../../data/tsDocs.json");
     let patternArray = [];
+
     patternData.children.map(item => {
         const filePath = item.name.replace(/"/g, "");
         const componentName = filePath.split("/")[1];
 
         if (
-            !filePath.includes("globals/") && 
+            !filePath.includes("globals/") &&
             validComponent(componentName)
         ) {
             patternArray.push({
@@ -19,7 +20,19 @@ const parseTsDocsData = () => {
     return patternArray;
 };
 
-const blackList = ["index", "withCopyAbility", "withCharacterCount", "-Docs", ".story"];
 const validComponent = name => name && blackList.every(bad => !name.includes(bad));
+
+const blackList = [
+    "index",
+    "withCopyAbility",
+    "withCharacterCount",
+    "-Docs",
+    ".story",
+    "Helpers",
+    "Styled",
+    "Variable",
+    "Types",
+    "Settings"
+];
 
 module.exports = parseTsDocsData;
