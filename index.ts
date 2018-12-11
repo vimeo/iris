313b41ef6458c4b6bcc885128a4d1ddd9f1f8348
Sean McIntyre <sean.mcintyre@vimeo.com>
2018-12-11 15:38:58 -0500

    5.18.1-alpha.1

--- index.ts
+++
@@ -1,8 +0,0 @@
-export * from './dist/';
-export * from './dist/globals/js/constants';
-
-import * as icons from './dist/icons/';
-import * as illustrations from './dist/illustrations/';
-import * as typography from './dist/Type';
-
-export { icons, illustrations, typography };
