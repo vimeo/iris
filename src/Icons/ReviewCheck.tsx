2714dc87b10b622d08dc065d04f352042077874f
Sean McIntyre <sean.mcintyre@vimeo.com>
2019-01-29 14:16:27 -0500

    add three new icons (#680)
    * add new icons
    
    * re-add new icons with correct dir case
    
    * Delete FileTransfer.tsx
    
    * Delete ReviewCheck.tsx
    
    * Delete SlackMark.tsx
    
    * Delete index.ts

---
+++ src/Icons/ReviewCheck.tsx
@@ -0,0 +1,18 @@
+import React, { SVGProps } from 'react';
+export const ReviewCheck = (props: SVGProps<SVGSVGElement>) => (
+    <svg viewBox="0 0 20 20" {...props}>
+        <path
+            d="M19 18.61V2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12.54l4.29 3.69a.1.1 0 0 0 .17-.08z"
+            fill="none"
+            stroke="#1a2e3b"
+            strokeWidth={2}
+        />
+        <path
+            fill="none"
+            stroke="#1a2e3b"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M13.5 5.5l-4.39 4.75L6.5 7.5"
+        />
+    </svg>
+);
