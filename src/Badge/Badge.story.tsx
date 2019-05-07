ab1ea5c96af17844b477b17d704cb10d22ec0cf5
Sean McIntyre <sean.mcintyre@vimeo.com>
2019-05-07 13:54:58 -0400

    PropsClean: Badge (#780)
    * covert badgeColorsCSS to switch
    
    * convert Badge to IrisComponent and consolidate styled code
    
    * arrange badge story vertically

--- src/Badge/Badge.story.tsx
+++ src/Badge/Badge.story.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Badge as B } from '../';
+import { Badge as B } from './Badge';
 import { storiesOf } from '@storybook/react';
 import { Story } from '../../.storybook/ui/Story';
 import styled from 'styled-components';
 import { select } from '@storybook/addon-knobs';
-import { dark } from '../../.storybook/theme';
 
 const Badge = styled(B)`
-  margin: 0.5rem 1rem;
+  margin: 0 100% 1.5rem 0;
 `;
 
 const sizes = {
@@ -15,152 +14,103 @@ const sizes = {
   lg: 'lg',
 };
 
-const componentName = 'Badge';
-
-storiesOf(`components|Badges`, module)
-  .addParameters({ options: { theme: (document as any).sbTheme } })
-
-  .add('Badge', props => {
-    // console.dir(props);
-
-    return (
-      <Story title={componentName}>
-        <Badge href="#" size={select('size', sizes, 'sm')}>
-          default
-        </Badge>
-        <Badge
-          href="#"
-          format="alum"
-          size={select('size', sizes, 'sm')}
-        >
-          alum
-        </Badge>
-        <Badge
-          href="#"
-          format="beta"
-          size={select('size', sizes, 'sm')}
-        >
-          beta
-        </Badge>
-        <Badge
-          href="#"
-          format="business"
-          size={select('size', sizes, 'sm')}
-        >
-          business
-        </Badge>
-        <Badge
-          href="#"
-          format="curation"
-          size={select('size', sizes, 'sm')}
-        >
-          curation
-        </Badge>
-        <Badge
-          href="#"
-          format="featured"
-          size={select('size', sizes, 'sm')}
-        >
-          featured
-        </Badge>
-        <Badge
-          href="#"
-          format="hd"
-          size={select('size', sizes, 'sm')}
-        >
-          hd
-        </Badge>
-        <Badge
-          href="#"
-          format="info"
-          size={select('size', sizes, 'sm')}
-        >
-          info
-        </Badge>
-        <Badge
-          href="#"
-          format="live"
-          size={select('size', sizes, 'sm')}
-        >
-          live
-        </Badge>
-        <Badge
-          href="#"
-          format="live-archive"
-          size={select('size', sizes, 'sm')}
-        >
-          live
-        </Badge>
-        <Badge
-          href="#"
-          format="new"
-          size={select('size', sizes, 'sm')}
-        >
-          new
-        </Badge>
-        <Badge
-          href="#"
-          format="partner"
-          size={select('size', sizes, 'sm')}
-        >
-          partner
-        </Badge>
-        <Badge
-          href="#"
-          format="plus"
-          size={select('size', sizes, 'sm')}
-        >
-          plus
-        </Badge>
-        <Badge
-          href="#"
-          format="producer"
-          size={select('size', sizes, 'sm')}
-        >
-          producer
-        </Badge>
-        <Badge
-          href="#"
-          format="pro"
-          size={select('size', sizes, 'sm')}
-        >
-          pro
-        </Badge>
-        <Badge
-          href="#"
-          format="sponsor"
-          size={select('size', sizes, 'sm')}
-        >
-          sponsor
-        </Badge>
-        <Badge
-          href="#"
-          format="staff"
-          size={select('size', sizes, 'sm')}
-        >
-          staff
-        </Badge>
-        <Badge
-          href="#"
-          format="support"
-          size={select('size', sizes, 'sm')}
-        >
-          support
-        </Badge>
-        <Badge
-          href="#"
-          format="upgrade"
-          size={select('size', sizes, 'sm')}
-        >
-          upgrade
-        </Badge>
-        <Badge
-          href="#"
-          format="vod"
-          size={select('size', sizes, 'sm')}
-        >
-          vod
-        </Badge>
-      </Story>
-    );
-  });
+storiesOf(`components|Badges`, module).add('Badge', () => (
+  <Story title="Badge">
+    <Badge href="#" size={select('size', sizes, 'sm')}>
+      default
+    </Badge>
+    <Badge href="#" format="alum" size={select('size', sizes, 'sm')}>
+      alum
+    </Badge>
+    <Badge href="#" format="beta" size={select('size', sizes, 'sm')}>
+      beta
+    </Badge>
+    <Badge
+      href="#"
+      format="business"
+      size={select('size', sizes, 'sm')}
+    >
+      business
+    </Badge>
+    <Badge
+      href="#"
+      format="curation"
+      size={select('size', sizes, 'sm')}
+    >
+      curation
+    </Badge>
+    <Badge
+      href="#"
+      format="featured"
+      size={select('size', sizes, 'sm')}
+    >
+      featured
+    </Badge>
+    <Badge href="#" format="hd" size={select('size', sizes, 'sm')}>
+      hd
+    </Badge>
+    <Badge href="#" format="info" size={select('size', sizes, 'sm')}>
+      info
+    </Badge>
+    <Badge href="#" format="live" size={select('size', sizes, 'sm')}>
+      live
+    </Badge>
+    <Badge
+      href="#"
+      format="live-archive"
+      size={select('size', sizes, 'sm')}
+    >
+      live
+    </Badge>
+    <Badge href="#" format="new" size={select('size', sizes, 'sm')}>
+      new
+    </Badge>
+    <Badge
+      href="#"
+      format="partner"
+      size={select('size', sizes, 'sm')}
+    >
+      partner
+    </Badge>
+    <Badge href="#" format="plus" size={select('size', sizes, 'sm')}>
+      plus
+    </Badge>
+    <Badge
+      href="#"
+      format="producer"
+      size={select('size', sizes, 'sm')}
+    >
+      producer
+    </Badge>
+    <Badge href="#" format="pro" size={select('size', sizes, 'sm')}>
+      pro
+    </Badge>
+    <Badge
+      href="#"
+      format="sponsor"
+      size={select('size', sizes, 'sm')}
+    >
+      sponsor
+    </Badge>
+    <Badge href="#" format="staff" size={select('size', sizes, 'sm')}>
+      staff
+    </Badge>
+    <Badge
+      href="#"
+      format="support"
+      size={select('size', sizes, 'sm')}
+    >
+      support
+    </Badge>
+    <Badge
+      href="#"
+      format="upgrade"
+      size={select('size', sizes, 'sm')}
+    >
+      upgrade
+    </Badge>
+    <Badge href="#" format="vod" size={select('size', sizes, 'sm')}>
+      vod
+    </Badge>
+  </Story>
+));
--- src/Badge/Badge.tsx
+++ src/Badge/Badge.tsx
@@ -1,46 +1,80 @@
-import React, { SFC } from 'react';
-import { BadgeStyled } from './BadgeStyled';
-import { BaseProps } from '../Utils/BaseProps';
+import React from 'react';
+import styled from 'styled-components';
+import { rem } from 'polished';
+import { badgeColorsCSS, badgeSizeCSS } from './BadgeStyleSettings';
+import { IrisComponent } from '../Utils/IrisComponent';
+import { fontFamily } from '../Typography/Typography';
 
-export const Badge: SFC<Props> = ({
-  children,
-  format = 'default',
-  href,
-  label,
-  size = 'sm',
-  ...props
-}) => (
-  <BadgeStyled href={href} format={format} size={size} {...props}>
-    {children}
-  </BadgeStyled>
-);
-
-export interface Props extends BaseProps {
-  format?:
-    | 'alum'
-    | 'beta'
-    | 'business'
-    | 'curation'
-    | 'default'
-    | 'explicit'
-    | 'featured'
-    | 'hd'
-    | 'info'
-    | 'live'
-    | 'live-archive'
-    | 'new'
-    | 'partner'
-    | 'plus'
-    | 'producer'
-    | 'pro'
-    | 'spatial'
-    | 'sponsor'
-    | 'staff'
-    | 'support'
-    | 'unrated'
-    | 'upgrade'
-    | 'vod';
+interface Props {
+  format?: BadgeFormats;
   href?: string;
   label?: string;
   size?: 'sm' | 'lg';
 }
+
+export const Badge: IrisComponent<Props> = ({
+  href,
+  format = 'default',
+  size = 'sm',
+  ...props
+}) => (
+  <BadgeStyled
+    as={href ? 'a' : 'span'}
+    format={format}
+    size={size}
+    {...props}
+  />
+);
+
+type BadgeFormats =
+  | 'alum'
+  | 'beta'
+  | 'business'
+  | 'curation'
+  | 'default'
+  | 'explicit'
+  | 'featured'
+  | 'hd'
+  | 'info'
+  | 'live'
+  | 'live-archive'
+  | 'new'
+  | 'partner'
+  | 'plus'
+  | 'producer'
+  | 'pro'
+  | 'spatial'
+  | 'sponsor'
+  | 'staff'
+  | 'support'
+  | 'unrated'
+  | 'upgrade'
+  | 'vod';
+
+const BadgeStyled = styled.span<{
+  format: BadgeFormats;
+  href?: string;
+  size: 'sm' | 'lg';
+}>`
+  display: inline-block;
+  padding: ${rem(3)} ${rem(4)};
+  font-family: ${fontFamily};
+  font-size: ${rem(9)};
+  font-weight: 700;
+  line-height: 1.2;
+  border-radius: ${rem(2)};
+  text-shadow: none;
+  text-align: center;
+  vertical-align: middle;
+  white-space: nowrap;
+  letter-spacing: 0.02rem;
+  text-transform: uppercase;
+
+  ${badgeSizeCSS};
+  ${badgeColorsCSS};
+
+  &::-moz-focus-inner {
+    padding: 0;
+    border: 0;
+  }
+`;
