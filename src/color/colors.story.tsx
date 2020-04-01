0e7c1c54a937bcec57b7836af6865fa5b82e48db
Sean Costello <sean.costello@vimeo.com>
2020-04-01 09:32:56 -0400

    Add story to compare new colors module to legacy color constants (#1018)
    * Add story to compare new colors module to legacy color constants
    
    * Add headers and restyle

--- src/color/colors.story.tsx
+++ src/color/colors.story.tsx
@@ -46,7 +46,8 @@ storiesOf('color|colors', module)
   .add('green', ColorStory('blue', green))
   .add('blue', ColorStory('blue', blue))
   .add('slate', ColorStory('slate', slate))
-  .add('grayscale', ColorStory('grayscale', grayscale));
+  .add('grayscale', ColorStory('grayscale', grayscale))
+  .add('compare', ColorCompareStory);
 
 function ColorStory(colorName, colorFn) {
   return () => {
@@ -100,6 +101,139 @@ function ColorStory(colorName, colorFn) {
       </Story>
     );
   };
+}
+
+const LEGCAY_COLORS = {
+  VimeoBlue: '#00adef',
+  VimeoBlueDarkened: '#0088cc',
+  VimeoBlueLightened: '#36C5FC',
+  DarkerBlue: '#093AC9',
+  Foam: '#e5f7fd',
+  Pistachio: '#4EC437',
+  PistachioDarkened: '#398F29',
+  PistachioLightened: '#7ED46C',
+  RumSwizzle: '#f2f9e5',
+  SunsetOrange: '#ff4d4d',
+  SunsetOrangeDarkened: '#d93636',
+  PalePink: '#ffeded',
+  AstroGranite: '#1a2e3b',
+  RegentGray: '#8498A4',
+  SoutherlySky: '#b3bfc8',
+  Porcelain: '#e3e8e9',
+  Plaster: '#eef1f2',
+  Paste: '#f6f7f8',
+  White: '#fff',
+  WarningYellow: '#ffb21e',
+  DarkDark: '#2E2E2E',
+  Black: '#000',
+  RavenImperial: '#1f1f1f',
+  ObsidianSlate: '#2e2e2e',
+  SovereignShadow: '#404040',
+  AshenWinter: '#4b4b4b',
+  IronHeart: '#666',
+};
+
+const REDS = ['PalePink', 'SunsetOrange', 'SunsetOrangeDarkened'];
+const YELLOWS = ['WarningYellow'];
+const BLUES = [
+  'Foam',
+  'VimeoBlueLightened',
+  'VimeoBlue',
+  'VimeoBlueDarkened',
+];
+const GREENS = [
+  'RumSwizzle',
+  'PistachioLightened',
+  'Pistachio',
+  'PistachioDarkened',
+];
+const SLATES = [
+  'Paste',
+  'Porcelain',
+  'SoutherlySky',
+  'RegentGray',
+  'AstroGranite',
+];
+const GRAYSCALE = [
+  'Plaster',
+  'IronHeart',
+  'AshenWinter',
+  'SovereignShadow',
+  'ObsidianSlate',
+  'RavenImperial',
+];
+
+
+function ColorCompare({ prior, current, title, weights = [50, 500, 600] }) {
+  return (
+    <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%' }}>
+      <Header>{title}</Header>
+      <div style={{ display: 'flex', width: '100%' }}>
+        {prior.map((colorName, i) => (
+          <ColorBlock
+            colorName={colorName}
+            colorHex={LEGCAY_COLORS[colorName]}
+            width="calc(10rem + 3vw)"
+            height="calc(10rem + 3vw)"
+            key={i}
+          />
+        ))}
+      </div>
+      <div style={{ display: 'flex', width: '100%' }}>
+        {weights.map((weight, i) => (
+          <ColorBlock
+            colorName={weight}
+            colorHex={current(weight)}
+            width="calc(10rem + 3vw)"
+            height="calc(10rem + 3vw)"
+            key={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ColorCompareStory() {
+  return (
+    <Story title="Compare legacy colors" width="100%" flex>
+      <ColorCompare
+        prior={REDS}
+        current={red}
+        title="red"
+      />
+      <ColorCompare
+        prior={YELLOWS}
+        current={yellow}
+        weights={[500]}
+        title="yellow"
+      />
+      <ColorCompare
+        prior={GREENS}
+        current={green}
+        weights={[50, 400, 500, 600]}
+        title="green"
+      />
+      <ColorCompare
+        prior={BLUES}
+        current={blue}
+        weights={[50, 400, 500, 600]}
+        title="blue"
+      />
+      <ColorCompare
+        prior={SLATES}
+        current={slate}
+        weights={[50, 100, 200, 500, 800]}
+        title="slate"
+      />
+      <ColorCompare
+        prior={GRAYSCALE}
+        current={grayscale}
+        weights={[50, 500, 650, 700, 800, 850]}
+        title="grayscale"
+      />
+    </Story>
+  )
 }
 
 export const ColorPalette: SFC<any> = ({
@@ -165,8 +299,13 @@ const ColorSwatchStyled = styled.div`
   margin-right: 0.25rem;
 `;
 
-const ColorBlock: SFC<any> = ({ colorName, colorHex }) => (
-  <ColorBlockStyled colorHex={colorHex}>
+const ColorBlock: SFC<any> = ({
+    colorName,
+    colorHex,
+    width = 'calc(13rem + 6vw)',
+    height = 'calc(12rem + 6vw)',
+}) => (
+  <ColorBlockStyled colorHex={colorHex} width={width} height={height}>
     <ColorHex>{colorHex}</ColorHex>
     <ColorTitle>{colorName}</ColorTitle>
   </ColorBlockStyled>
@@ -204,8 +343,8 @@ const ColorHex = styled.span`
 
 const ColorBlockStyled = styled.div<any>`
   background: ${p => p.colorHex};
-  width: calc(13rem + 6vw);
-  height: calc(12rem + 6vw);
+  width: ${p => p.width};
+  height: ${p => p.height};
   margin: 1rem;
   float: left;
   border-radius: 3px;
