34681ae3e338f570ffaa76a42a278205037c48b1
Alice Xia <alice.xia@vimeo.com>
2020-05-19 16:19:35 -0400

    pass value (#1113)
    Co-authored-by: Sean McIntyre <sean.mcintyre@vimeo.com>

--- src/components/inputs/Input/Mark.tsx
+++ src/components/inputs/Input/Mark.tsx
@@ -56,6 +56,7 @@ export function Mark({
           disabled={disabled}
           ref={forwardRef}
           defaultValue={defaultValue as string | string[]}
+          value={value as string | string[]}
           {...props}
         />
         <Label
