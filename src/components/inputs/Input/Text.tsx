cefaea26dfb9cbe1c08827525e5e460f2e4a2927
Alice Xia <alice.xia@vimeo.com>
2020-06-17 14:56:51 -0400

    check if inputRef.current exists (#1146)
    Co-authored-by: Sean McIntyre <sean.mcintyre@vimeo.com>

--- src/components/inputs/Input/Text.tsx
+++ src/components/inputs/Input/Text.tsx
@@ -43,7 +43,7 @@ export function Text({
 
   const floatLabel =
     focused ||
-    (inputRef.current && inputRef.current.value.length > 0);
+    (inputRef.current && inputRef.current.value?.length > 0);
 
   return (
     <Wrapper
