ec370834ded19f0373370c1b221312a52a732e71
Alice Xia <alice.xia@vimeo.com>
2020-10-26 17:57:06 -0400

    update annotation tip

--- src/components/info/Annotation/Annotation.style.ts
+++ src/components/info/Annotation/Annotation.style.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
-import { CircleInfo } from '../../../icons';
 import { slate, blue } from '../../../color';
 
 export const Annotation = styled.div`
-  display: inline-block;
+  display: flex;
+  align-items: flex-start;
   position: relative;
   color: ${slate(500)};
 
@@ -14,12 +14,4 @@ export const Annotation = styled.div`
   * {
     fill: currentColor;
   }
-`;
-
-export const Icon = styled(CircleInfo)`
-  position: absolute;
-  top: 0;
-  right: -1.25rem;
-  width: 1rem;
-  height: 1rem;
 `;
