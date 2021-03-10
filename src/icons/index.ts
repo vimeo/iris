9448cb03a975c0dd2360e3b5a32891114c3d1a95
alice.xia@vimeo.com <alice.xia@vimeo.com>
2021-03-10 13:24:25 -0500

    export playFilled (#1305)

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -124,6 +124,7 @@ export { Person } from './ui/Person';
 export { PersonFilled } from './ui/PersonFilled';
 export { Pin } from './ui/Pin';
 export { Play } from './ui/Play';
+export { PlayFilled } from './ui/PlayFilled';
 export { Plus } from './ui/Plus';
 export { PopIn } from './ui/PopIn';
 export { PopOut } from './ui/PopOut';
