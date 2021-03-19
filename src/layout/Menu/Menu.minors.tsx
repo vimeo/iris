2dd3e40aa8d697b4c9d2f2f0f117f9c6bb5f9676
Sean McIntyre <sean.mcintyre@vimeo.com>
2021-03-19 14:08:43 -0400

    split Menu.Item minor into two components

--- src/layout/Menu/Menu.minors.tsx
+++ src/layout/Menu/Menu.minors.tsx
@@ -26,52 +26,74 @@ export function Section({ children, title = null, ...props }) {
   );
 }
 
-export function Item({
-  action,
-  active,
-  children,
-  href,
-  toggle = false,
-  icon,
-  ...props
-}) {
-  const [open, setOpen] = useState(!toggle);
-
-  const doToggle = () => toggle && setOpen((open) => !open);
-
-  const doAction = (e: MouseEvent) => {
-    action.onClick && action.onClick(e);
-  };
-
-  const simpleKids =
+export function Item({ children, ...props }) {
+  const simple =
     typeof children === 'object' && !children.props
       ? children.filter((child) => typeof child === 'string')
       : children;
 
-  const complexKids =
+  const complex =
     typeof children === 'object' &&
     !children.props &&
     children.filter((child) => typeof child !== 'string');
 
-  const link = simpleKids && href;
+  if (complex) {
+    const children = { simple, complex };
+    return <ComplexItem {...props}>{children}</ComplexItem>;
+  }
+
+  if (simple) {
+    const children = simple;
+    return <SimpleItem {...props}>{children}</SimpleItem>;
+  }
+}
+
+function SimpleItem({
+  active,
+  children,
+  href,
+  icon,
+  action,
+  ...props
+}) {
+  const onClick = (e: MouseEvent) => {
+    action.onClick && action.onClick(e);
+  };
 
   return (
-    <Wrapper active={active} $height={open && complexKids.length}>
-      <ItemStyled
-        onClick={doToggle}
-        as={link ? 'a' : 'button'}
-        href={href}
-        {...props}
-      >
+    <Wrapper active={active}>
+      <ItemStyled as={href ? 'a' : 'button'} href={href} {...props}>
         {action && (
-          <Action onClick={doAction}>
+          <Action onClick={onClick}>
             {action.icon}
             <Focus parent={Action} />
           </Action>
         )}
 
         {icon && icon}
-        {simpleKids}
+        {children}
+        <Focus parent={ItemStyled} />
+      </ItemStyled>
+    </Wrapper>
+  );
+}
+
+function ComplexItem({
+  active,
+  children,
+  icon,
+  toggle = false,
+  ...props
+}) {
+  const [open, setOpen] = useState(!toggle);
+
+  const onClick = () => toggle && setOpen((open) => !open);
+
+  return (
+    <Wrapper active={active} $height={open && children.length}>
+      <ItemStyled onClick={onClick} {...props}>
+        {icon && icon}
+        {children.simple}
         <Focus parent={ItemStyled} />
       </ItemStyled>
 
@@ -81,7 +103,8 @@ export function Item({
           <Focus parent={Toggle} />
         </Toggle>
       )}
-      {open && complexKids && <SubMenu>{complexKids}</SubMenu>}
+
+      {open && <SubMenu>{children.complex}</SubMenu>}
     </Wrapper>
   );
 }
