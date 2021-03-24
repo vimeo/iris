b7acebbcf3d0e25bec28b15adbbcbe56bef21ef5
alice.xia@vimeo.com <alice.xia@vimeo.com>
2021-03-24 13:16:44 -0400

    export panel (#1323)
    Co-authored-by: sean.mcintyre@vimeo.com <sean.mcintyre@vimeo.com>

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -3,6 +3,7 @@ export { Grid } from './Grid/Grid';
 export { Menu } from './Menu/Menu';
 export { Modal } from './Modal/Modal';
 export { Pagination } from './Pagination/Pagination';
+export { Panel } from './Panel/Panel';
 export { Pop, PopOver } from './PopOver/PopOver';
 export { Tabs } from './Tabs/Tabs';
 export { Ribbon } from './Ribbon/Ribbon';
