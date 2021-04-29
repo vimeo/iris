9fb861e52f4930fcd35d2a9b1022eec70b5c0d4f
Sean McIntyre <sean.mcintyre@vimeo.com>
2021-04-29 08:49:48 -0400

    delete copy

--- src/layout/Panel/examples/TranscriptPanel copy/TranscriptTabs/Caption/delay.ts
+++
@@ -1,7 +0,0 @@
-const delays = [];
-
-const logBase = (n = 1, base = 10) => Math.log(n) / Math.log(base);
-const calcDelay = (i: number) => logBase(i, 1.1) * 10;
-
-for (let i = 1; i < 30; i++) delays[i] = Math.round(calcDelay(i));
-export const delay = ({ delay }) => delays[delay];
