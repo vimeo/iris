cf9255e21c16bef053ea0a963634b5161e82cee0
Sean McIntyre <seanmcintyre@gmail.com>
2021-05-25 14:01:31 -0400

    Toolbar: export from '@vimeo/iris/layout'

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -4,9 +4,10 @@ export { Menu } from './Menu/Menu';
 export { Modal } from './Modal/Modal';
 export { Pagination } from './Pagination/Pagination';
 export { Panel } from './Panel/Panel';
-export { PopOver } from './PopOver/PopOver';
 export { Pop } from './PopOver/PopOver.minors';
-export { Tabs } from './Tabs/Tabs';
+export { PopOver } from './PopOver/PopOver';
 export { Ribbon } from './Ribbon/Ribbon';
+export { Tabs } from './Tabs/Tabs';
+export { Toolbar } from './Toolbar/Toolbar';
 
 export { VirtualizedList } from './VirtualizedList/VirtualizedList';
