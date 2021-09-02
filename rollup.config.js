44e38e8294ccc8e65bd8f4c4cc869b0a7ccc5052
Christian de Botton <{ID}+{username}@users.noreply.github.com>
2021-09-01 21:50:20 -0700

    Update prepublish script

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,6 @@ import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
@@ -42,10 +41,9 @@ export default (args) => {
       commonjs(),
       resolve(),
       typescript(),
-      babel({ exclude: '**/node_modules' }),
+      babel({ exclude: '**/node_modules', babelHelpers: 'runtime' }),
       multi(),
-      !debug && terser({ mangle: true, compress: { passes: 2 } }),
-    ].filter(Boolean),
+    ],
   };
 
   return config;
