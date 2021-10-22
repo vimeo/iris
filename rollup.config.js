f2209c88b11e61024b26788147df62c58b353648
Christian de Botton <{ID}+{username}@users.noreply.github.com>
2021-10-22 14:43:11 -0700

    Add add-pkgs script

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,41 @@
-import { terser } from 'rollup-plugin-terser';
-
+import multi from 'rollup-plugin-multi-input';
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import multiInput from 'rollup-plugin-multi-input';
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
 
 import pkg from './package.json';
 
-const dependencies = Object.keys(pkg.dependencies || {});
-const peerDependencies = Object.keys(pkg.peerDependencies || {});
-const babelRuntime = (id) => id.includes('@babel/runtime');
-
 export default (args) => {
-  const debug = args['config-debug'];
   process.env.NODE_ENV = 'production';
 
-  return {
-    input: [
+  const debug = args['config-debug'];
+
+  /** @type {import('rollup').RollupOptions} */
+  let config = {
+    input:
       './src/!(_|storybook)*/**/!(*.story.tsx|*.test.ts|*.types.ts)',
+    output: [
+      {
+        dir: 'build',
+        format: 'esm',
+        entryFileNames: '[name].esm.js',
+      },
+      {
+        dir: 'build',
+        format: 'commonjs',
+        entryFileNames: '[name].js',
+      },
     ],
-    output: {
-      dir: './build',
-      format: 'cjs',
-    },
-    external: [...dependencies, ...peerDependencies, babelRuntime],
+    external: Object.keys(pkg.peerDependencies),
     plugins: [
-      resolve(),
       commonjs(),
+      resolve(),
       typescript(),
-      babel({
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        babelHelpers: 'runtime',
-        exclude: /node_modules/,
-      }),
-      !debug && terser(),
-      multiInput(),
-    ].filter((p) => p),
+      babel({ exclude: '**/node_modules', babelHelpers: 'runtime' }),
+      multi(),
+    ],
   };
+
+  return config;
 };
