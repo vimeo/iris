c4f324b3bda280321ddafbdedfbfd42b579b0f84
Sean McIntyre <seanmcintyre@gmail.com>
2021-12-14 16:18:14 -0500

    WIP

--- src/Image.story.tsx
+++ src/Image.story.tsx
@@ -1,52 +1,62 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+import { blue } from '../src/color';
+
 export default { title: 'Image' };
 
 export function ImageStory() {
   return (
-    <Img src="https://i.vimeocdn.com/video/330051511313_288x288" />
+    <>
+      <Img src="https://i.vimeocdn.com/video/1147513349-cb9702b73274b7adfd614d50bff1c7069b3825048e683e35a2b0364b4f180549-d_472x266" />
+      <Img src="https://i.vimeocdn.com/video/1325655366-d69d494982ee1a8d1fea22f4034aee9efe0c5b583911a0d116659f303bd0155c-d_295x166?r=pad" />
+    </>
   );
 }
 
 function Img({ src }) {
-  const ImageData = new Image();
-  ImageData.src = src;
-  console.dir({ ImageData });
+  const [status, loading] = useFetchImage(src);
+  console.log({ status, loading });
 
-  loadImage(src);
-
-  return <img src={src} />;
+  return status !== '404' ? <img src={src} /> : <Placeholder />;
 }
 
-async function loadImage(src) {
-  // const headers = { etag: '3f79bb6f12fe2e5e1226b4f1a1a819e4' };
-  console.log('\n\n');
+const Placeholder = styled.div`
+  margin: 2rem 0;
+  width: 472px;
+  height: 266px;
+  border: 2px solid ${blue(500)};
+  color: ${blue(500)};
+  padding: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
 
-  const response = await fetch(src, {
-    method: 'GET',
-    // cache: 'no-cache',
-    // headers,
-  });
-
-  // .then((data) => {
-  //   console.log('\n', { data });
-  //   for (const pair of data.headers.entries()) {
-  //     console.log(pair[0] + ': ' + pair[1]);
-  //   }
-  // });
-
-  for (const [key, value] of response.headers) {
-    console.log(`${key} = ${value}`);
+  &:before {
+    content: 'I can be anything!';
   }
+`;
 
-  const length = response.headers.get('content-length');
-  const type = response.headers.get('content-type');
-  const test = response.headers.get('x-viewmaster-lossless-format');
-  console.log({ test });
+function useFetchImage(src) {
+  const [status, statusSet] = useState(null);
+  const [loading, loadingSet] = useState(true);
 
-  if (length === '23568' && type === 'image/jpeg') {
-    console.log('default!');
-  } else {
-    console.log('not default!');
-  }
+  useEffect(() => {
+    async function fetchImage() {
+      try {
+        loadingSet(true);
+        const response = await fetch(src);
+        const status = response.headers.get('x-viewmaster-status');
 
-  // return data;
+        statusSet(status);
+        loadingSet(false);
+      } catch (error) {
+        loadingSet(false);
+      }
+    }
+    fetchImage();
+  }, [src]);
+
+  return [status, loading];
 }
