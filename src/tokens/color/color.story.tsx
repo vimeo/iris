c5c638cfd55168409420ad53e4bbce3845d92ae4
Sean McIntyre <seanmcintyre@gmail.com>
2022-02-07 19:06:17 -0500

    export new tokens

--- src/tokens/color/color.story.tsx
+++ src/tokens/color/color.story.tsx
@@ -2,7 +2,9 @@ export default { title: 'Tokens/color' };
 
 export { Tokens as Background } from './background/background.story';
 export { Tokens as Format } from './format/format.story';
+export { Tokens as Rainbow } from './rainbow/rainbow.story';
 export { Tokens as Status } from './status/status.story';
 export { Tokens as Stroke } from './stroke/stroke.story';
 export { Tokens as Surface } from './surface/surface.story';
 export { Tokens as Text } from './text/text.story';
+export { Tokens as Upsell } from './upsell/upsell.story';
--- src/tokens/color/index.ts
+++ src/tokens/color/index.ts
@@ -1,15 +1,19 @@
 import { background } from './background/background';
 import { format } from './format/format';
+import { rainbow } from './rainbow/rainbow';
 import { status } from './status/status';
 import { stroke } from './stroke/stroke';
 import { surface } from './surface/surface';
 import { text } from './text/text';
+import { upsell } from './upsell/upsell';
 
 export const color = {
   background,
   format,
+  rainbow,
   status,
   stroke,
   surface,
   text,
+  upsell,
 };
