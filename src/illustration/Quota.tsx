eb973e23e5131abae5a46d73c4f5ef435b305fce
Alice Xia <alicexia20@gmail.com>
2022-02-11 12:12:42 -0500

    quota illustration (#86)
    Co-authored-by: Alice Xia <alice.xia@vimeo.com>

---
+++ src/illustration/Quota.tsx
@@ -0,0 +1,11 @@
+import React, { SVGProps } from 'react';
+export const Quota = (props: SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 99 51" {...props}>
+    <path d="M91 3h3v11h-3zM82 40h3v11h-3zM4 18h3v11H4z" />
+    <path d="M87 7h11v3H87zM78 44h11v3H78zM0 22h11v3H0z" />
+    <g>
+      <path d="M32.661 0v7.648h-7.83v7.647H17V48h48.339v-7.648h7.83V35.61H81V0H32.661zm44.574 31.933H73.17V7.648H36.426V3.677h40.81v28.256zm-7.83-.313v5.037h-4.066V15.295H28.595v-3.97h40.81V31.62zm-7.83 5v7.685h-40.81V18.972h40.809V36.62z" />
+      <path d="M33.941 38.514v1.838h23.191v-3.676h-23.19v1.838zM25.227 40.83l5.044-2.849-5.044-2.85v5.7z" />
+    </g>
+  </svg>
+);
