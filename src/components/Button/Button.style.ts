7d82e0cd918f62e55198ea51c1f3a2687fa93398
Sean McIntyre <seanmcintyre@gmail.com>
2022-04-20 16:40:22 -0400

    Button: update transparent variant styles

--- src/components/Button/Button.style.ts
+++ src/components/Button/Button.style.ts
@@ -302,27 +302,32 @@ function buttonVariants({
       return css`
         border: ${borderWidth} solid transparent;
         background: transparent;
+        backdrop-filter: blur(0px);
         color: ${color};
 
         &:hover {
           border: ${borderWidth} solid transparent;
           background: ${rgba(color, 0.1)};
+          backdrop-filter: blur(1.5px);
           /* color: hoverColorDark */
           color: ${hoverColor};
         }
       `;
     case 'transparent':
       return css`
-        background: ${rgba(color, 0.35)};
+        background: ${rgba(color, 0.6)};
+        backdrop-filter: blur(4px);
         color: ${white};
 
         &:active {
-          background: ${rgba(shade(0.2, activeColor), 0.55)};
+          background: ${rgba(shade(0.2, activeColor), 0.75)};
+          backdrop-filter: blur(4px);
           transform: scale(0.98);
         }
 
         &:hover:not(:active) {
-          background: ${rgba(color, 0.45)};
+          background: ${rgba(color, 0.675)};
+          backdrop-filter: blur(4px);
           /* if: grow */
           /* transform: scale(1.01); */
           /* box-shadow: 0 0 0 0 rgba(black, 0.1); */
