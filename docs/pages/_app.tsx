78f9e07d1573734816368f378fce8fa6cb4487a8
Chris Schnaars <chris.schnaars@vimeo.com>
2022-05-26 09:01:33 -0400

    Create temporary space tokens

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -21,6 +21,26 @@ export default function App({ Component, pageProps }) {
 const GlobalStylesApp = createGlobalStyle`
   :root {
     --layout-site-width: 60rem;
+    --layout-sidebar-width: 16rem;
+    --space-0: 0;
+    --space-50: 0.25rem;
+    --space-100: 0.5rem;
+    --space-150: 0.75rem;
+    --space-200: 1rem;
+    --space-250: 1.25rem;
+    --space-300: 1.5rem;
+    --space-400: 2rem;
+    --space-500: 2.5rem;
+    --space-600: 3rem;
+    --space-700: 3.5rem;
+    --space-800: 4rem;
+    --space-900: 4.5rem;
+    --space-1000: 5rem;
+    --space-1200: 6rem; 
+  }
+
+  img {
+    width: 100%;
   }
   `;
 
