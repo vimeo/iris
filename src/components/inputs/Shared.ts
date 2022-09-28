d952e36f45478db5fbc8f5d550a1e11de4dfbdf4
Tyler Grove <tyler.grove@vimeo.com>
2022-09-28 13:50:29 -0600

    Null check process for dev warning

--- src/components/inputs/Shared.ts
+++ src/components/inputs/Shared.ts
@@ -9,8 +9,6 @@ import { rgba, rem } from 'polished';
 import { blue, white } from '../../color';
 import { Statuses } from '../../themes';
 
-const isDev = process?.env?.NODE_ENV === 'development';
-
 export function inputColors({ theme, disabled = false, format }) {
   if (!format || !theme.formats[format]) format = 'basic';
 
@@ -285,7 +283,7 @@ export function validate(
   const Name = type.charAt(0).toUpperCase() + type.slice(1);
   const valid = children.every(compareMetas(type));
 
-  if (!valid && isDev) {
+  if (!valid && process && process?.env?.NODE_ENV === 'development') {
     console.warn(
       `<${Name}Set /> children must be <${Name} />.`,
       children
