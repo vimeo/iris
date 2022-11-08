b6bc18baf5c95b1ba965467b1ea8b8c9b4958d54
Danil Chepelev <chepelevdi@gmail.com>
2022-11-08 10:17:30 +0200

    Update src/utils/general/mergeReactRefs.ts
    Co-authored-by: Julie Wongbandue <juliewongbandue@gmail.com>

--- src/utils/general/mergeReactRefs.ts
+++ src/utils/general/mergeReactRefs.ts
@@ -1,3 +1,4 @@
+// A utility function to merge refs, referenced from this library: https://www.npmjs.com/package/react-merge-refs
 export function mergeRefs<T = any>(
   refs: Array<React.MutableRefObject<T> | React.LegacyRef<T>>
 ): React.RefCallback<T> {
