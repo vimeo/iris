8b55ff8c6f1eddf2263f54daff732a9d51e64613
Kevin Graber <KJG310@gmail.com>
2023-05-19 10:15:45 -0400

    export meta typography component (#268)

--- src/typography/index.ts
+++ src/typography/index.ts
@@ -1,6 +1,7 @@
 export { BigStat } from './BigStat/BigStat';
 export { Header } from './Header/Header';
 export { Link } from './Link/Link';
+export { Meta } from './Meta/Meta';
 export { Paragraph } from './Paragraph/Paragraph';
 
 export { Text } from './Text/Text';
