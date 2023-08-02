6d2b5fa12a633d2314fdfeb01de3ad53d7e35946
Julie Wongbandue <juliewongbandue@gmail.com>
2023-08-02 13:11:49 -0400

    Add storybook-addon-pseudo-states, add button stickersheet

--- src/components/Button/Button.story.tsx
+++ src/components/Button/Button.story.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Story } from '@storybook/react';
+import { StoryFn } from '@storybook/react';
 import styled from 'styled-components';
 
 import { Button as B } from './Button';
 import { Props } from './Button.types';
 import { DownloadArrow } from '../../icons';
+import { Header } from '../../typography';
+
+import {
+  formats,
+  statuses,
+  variants,
+  sizes,
+  iconPositions,
+} from './Button.config';
 
 export default {
   title: 'components/Button',
@@ -28,7 +37,7 @@ const ButtonStyled = styled(B)`
   max-width: 10rem;
 `;
 
-const Template: Story<Props> = ({ children, ...args }) => {
+const Template: StoryFn<Props> = ({ children, ...args }) => {
   return (
     <>
       <ButtonStyled {...args} size="md">
@@ -44,4 +53,108 @@ const Template: Story<Props> = ({ children, ...args }) => {
 export const Button = Template.bind({});
 Button.args = {
   children: 'Button',
+};
+
+const states = ['default', 'hover', 'focus', 'active', 'disabled'];
+const formatStickers = formats.flatMap((format) =>
+  states.map((state) => ({ format: format, id: state }))
+);
+const variantStickers = variants.flatMap((variant) =>
+  states.map((state) => ({ variant: variant, id: state }))
+);
+const sizeStickers = sizes.flatMap((size) =>
+  states.map((state) => ({ size: size, id: state }))
+);
+const iconPositionStickers = iconPositions.flatMap((iconPosition) =>
+  states.map((state) => ({ iconPosition: iconPosition, id: state }))
+);
+
+const LayoutGrid = styled.div`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(5, 1fr);
+`;
+const GridCell = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+`;
+const StyledHeader = styled(Header)`
+  margin-top: 2rem;
+`;
+
+const Sheet = () => {
+  return (
+    <>
+      <StyledHeader>Formats</StyledHeader>
+      <LayoutGrid>
+        {formatStickers.map((sticker, i) => (
+          <GridCell key={i}>
+            <pre>{sticker.format}</pre>
+            <ButtonStyled
+              {...sticker}
+              disabled={sticker.id === 'disabled'}
+            >
+              {sticker.id}
+            </ButtonStyled>
+          </GridCell>
+        ))}
+      </LayoutGrid>
+      <StyledHeader>Variants</StyledHeader>
+      <LayoutGrid>
+        {variantStickers.map((sticker, i) => (
+          <GridCell key={i}>
+            <pre>{sticker.variant}</pre>
+            <ButtonStyled
+              {...sticker}
+              disabled={sticker.id === 'disabled'}
+            >
+              {sticker.id}
+            </ButtonStyled>
+          </GridCell>
+        ))}
+      </LayoutGrid>
+      <StyledHeader>Sizes</StyledHeader>
+      <LayoutGrid>
+        {sizeStickers.map((sticker, i) => (
+          <GridCell key={i}>
+            <pre>{sticker.size}</pre>
+            <ButtonStyled
+              {...sticker}
+              disabled={sticker.id === 'disabled'}
+            >
+              {sticker.id}
+            </ButtonStyled>
+          </GridCell>
+        ))}
+      </LayoutGrid>
+      <StyledHeader>Icon Positions</StyledHeader>
+      <LayoutGrid>
+        {iconPositionStickers.map((sticker, i) => (
+          <GridCell key={i}>
+            <pre>{sticker.iconPosition}</pre>
+            <ButtonStyled
+              {...sticker}
+              icon={<DownloadArrow />}
+              disabled={sticker.id === 'disabled'}
+            >
+              {sticker.id}
+            </ButtonStyled>
+          </GridCell>
+        ))}
+      </LayoutGrid>
+    </>
+  );
+};
+
+export const StickerSheet = Sheet.bind({});
+StickerSheet.parameters = {
+  pseudo: {
+    hover: '#hover',
+    focusVisible: '#focus',
+    active: '#active',
+  },
+  lostpixel: {
+    waitBeforeScreenshot: 4000,
+  },
 };
